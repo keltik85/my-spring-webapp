4c1d7bd4b5d3435f0b1e8da7f301008769bb6583
keltik85 <atekin@sts.de>
2017-02-07 17:09:06 +0100

    changed config parameters for unit testing db and added something to gitinore

--- test/com/caveofprogramming/spring/web/test/tests/UserDaoTests.java
+++ test/com/caveofprogramming/spring/web/test/tests/UserDaoTests.java
@@ -1,12 +1,21 @@
 package com.caveofprogramming.spring.web.test.tests;
 
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import com.caveofprogramming.spring.web.dao.User;
+import com.caveofprogramming.spring.web.dao.UsersDao;
 
 @ActiveProfiles("dev")
 @ContextConfiguration(locations = { "classpath:com/caveofprogramming/spring/web/config/dao-context.xml",
@@ -14,8 +23,25 @@ import static org.junit.Assert.*;
 		"classpath:com/caveofprogramming/spring/web/test/config/datasource.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserDaoTests {
+
+	@Autowired
+	private UsersDao usersDao;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Before
+	public void init() {
+		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+		jdbc.execute("delete from user_roles");
+		jdbc.execute("delete from users");
+		
+	}
+
 	@Test
 	public void testCreateUSer() {
-		assertEquals(false, false);
+		User user = new User("testcaseuser","heyhopassword","asdf@asdf.de", true, "ROLE_USER");
+		
+		assertTrue("User is created successfully", usersDao.create(user));
 	}
 }
